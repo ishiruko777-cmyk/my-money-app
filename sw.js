8285c1f634f1dd37d8bfc9ba3e74cb45bd388b6e
ishiruko777-cmyk <ishiruko777@gmail.com>
2026-08-18 19:00:58 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='today-kakei-v4';
+const CACHE='today-kakei-v6';
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./icon.svg'])).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>event.respondWith(caches.open(CACHE).then(cache=>cache.match(event.request).then(found=>found||fetch(event.request)))));
